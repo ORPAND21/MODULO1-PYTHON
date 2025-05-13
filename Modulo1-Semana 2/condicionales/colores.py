70f604bf5b92b18fe59ab7c3db472791fb3205ae
ORPAND21 <orpand21@gmail.com>
2025-05-13 09:19:49 -0500

    Se agrega el MODULO1-Python

---
+++ Modulo1-Semana 2/condicionales/colores.py
@@ -0,0 +1,12 @@
+colores = input("Ingresa un colocar ")
+
+if colores == "rojo":
+    print("Tu color es rojo")
+elif colores == "azul":
+    print("Tu color es azul")
+elif colores == "verde":
+    print("Tu color es verde")
+elif colores == "amarillo":
+    print("Tu color amarillo")
+else:
+    print("Color no reconocido")
