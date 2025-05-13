d917cb5f82c52f793629b5260c4b11ff6bd7448a
ORPAND21 <orpand21@gmail.com>
2025-05-13 07:55:59 -0500

    "Se agrego al MODULO1-Python
    conditional ejercicio

---
+++ Modulo1-Semana 2/condicionales/multiplo.py
@@ -0,0 +1,24 @@
+number = int(input("Ingresa un numero: "))
+
+if number % 2 == 0:
+    print("Tu numero es multiplo de 2")
+else:
+    print("Tu numero no es multiplo de 2")
+    
+if number % 3 == 0:
+    print("Tu numero  es multiplo de 3")
+else:
+    print("Tu numero no es multiplo de 3")
+
+if number % 5 == 0:
+    print("Tu numero es multiplo de 5")
+else:
+    print("Tu numero no es multiplo de 5")
+
+if number % 7 == 0:
+    print("Tu numero es multiplo de 7")
+else:
+    print("Tu numero no es multiplo de 7")
+    
+    
+print(number)
